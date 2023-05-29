a6326a766846c84e274a2f9e7bcf2b6557aa3f95
Rochalion <134647550+Rochalion@users.noreply.github.com>
2023-05-29 17:21:43 -0300

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/Home.css";
+import me from "../imgs/me.jpeg";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <img src={me} alt="Me" />
+        <h2>Giovanni Rocha</h2>
+        <div className="prompt">
+          <p>Um estudante da ETEC, aspirante a programação.</p>
+          <a
+            className="social-media"
+            href="https://www.linkedin.com/in/giovanni-rocha-78a88b268/"
+          >
+            Linkledin
+          </a>
+          <a className="social-media" href="https://github.com/Rochalion">
+            Github
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
